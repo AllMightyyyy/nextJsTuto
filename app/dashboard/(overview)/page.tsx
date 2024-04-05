561f3b9e1ab63f171504b6168d58c077bd3b58b9
AllMightyyyy <zakariafarih142@gmail.com>
2024-04-05 22:04:05 +0200

    used CardWrapper to make sure cards load sync and changed fetching from Dashboard page into the CardWrapper component

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,4 +1,4 @@
-import { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
@@ -6,21 +6,25 @@ import { fetchCardData } from '../../lib/data'; //remove fetchRevenue
 import { Suspense } from 'react';
 import { RevenueChartSkeleton } from '@/app/ui/skeletons';
 import { LatestInvoicesSkeleton } from '@/app/ui/skeletons';
+import { CardsSkeleton } from '@/app/ui/skeletons';
  
 export default async function Page() {
     //const revenue = await fetchRevenue();
     //const latestInvoices = await fetchLatestInvoices();
+    {/*
     const cardData = await fetchCardData();
     const numberOfCustomers = cardData.numberOfCustomers;
     const numberOfInvoices = cardData.numberOfInvoices;
     const totalPaidInvoices = cardData.totalPaidInvoices;
     const totalPendingInvoices = cardData.totalPendingInvoices;
+    */}
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/*
         <Card title="Collected" value={totalPaidInvoices} type="collected" /> 
         <Card title="Pending" value={totalPendingInvoices} type="pending" /> 
          <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> 
@@ -29,6 +33,10 @@ export default async function Page() {
           value={numberOfCustomers}
           type="customers"
         /> 
+        */}
+        <Suspense fallback={< CardsSkeleton/>}>
+          <CardWrapper />
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
